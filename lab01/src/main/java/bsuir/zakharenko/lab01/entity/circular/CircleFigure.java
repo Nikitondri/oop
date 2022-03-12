5dc199aeab38c32ab080e1b6f36a7217055930f1
Nikita <nikitazacharenko@mail.ru>
2022-03-12 18:59:41 +0300

    created lab02

--- lab01/src/main/java/bsuir/zakharenko/lab01/entity/circular/CircleFigure.java
+++ lab01/src/main/java/bsuir/zakharenko/lab01/entity/circular/CircleFigure.java
@@ -6,9 +6,9 @@ import main.java.bsuir.zakharenko.lab01.entity.Point;
 public class CircleFigure extends CircularFigure{
     private final double radius;
 
-    public CircleFigure(Point centre, double radius) {
+    public CircleFigure(Point centre, double newRadius) {
         super(FigureType.CIRCLE, centre);
-        this.radius = radius;
+        radius = newRadius;
     }
 
     public double getRadius() {
---
+++ lab02/src/main/bsuir/zakharenko/lab02/controller/Controller.java
@@ -0,0 +1,13 @@
+package main.bsuir.zakharenko.lab02.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+
+    }
+}
